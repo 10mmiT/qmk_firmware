6ff9f2aec35b9ee75ebf58983aca6e6dbff0fd05
Tim "TimmiT <timhaugstvedt@gmail.com>
2023-03-10 14:31:32 +0100

    Testing OLED with bongocat on Iris rev2

---
+++ keyboards/keebio/iris/keymaps/timmit-OLED/config.h
@@ -0,0 +1,14 @@
+#pragma once
+
+#define USE_SERIAL
+
+#define EE_HANDS
+
+#undef RGBLED_NUM
+#define RGBLED_NUM 12
+#undef RGBLED_SPLIT
+#define RGBLIGHT_SLEEP
+#define RGBLIGHT_LAYERS
+
+#define OLED_BRIGHTNESS 128
+#define OLED_DISPLAY_128X64
